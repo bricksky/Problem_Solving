f5cb00cb3173807f5a2cd587681cdc525adb17d2
bricksky <skydh1214@naver.com>
2023-04-03 12:34:55 +0900

    합

---
+++ Step01/8393.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a;
+    int total;
+
+    cin >> a;
+
+    total = (a * (a + 1)) / 2;
+
+    cout << total << endl;
+    return 0;
+}
