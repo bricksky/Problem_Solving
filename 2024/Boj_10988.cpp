ab63c82e3508a295c1d2e34c2e160cdd474f2d3b
bricksky <skydh1214@naver.com>
2024-09-17 11:52:26 +0900

    240917

---
+++ 2024/Boj_10988.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string input;
+    cin >> input;
+
+    for(int i=0; i<input.length(); i++){
+        if(input[i] == input[input.length()-(i+1)]){
+            cout << "1";
+            return 0;
+        }
+        cout << "1";
+    }
+}
