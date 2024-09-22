abcd12726ad2049bc6686accf5cad7106f2096c5
bricksky <skydh1214@naver.com>
2024-09-22 15:14:16 +0900

    240917

--- 2024/Boj_10988.cpp
+++ 2024/Boj_10988.cpp
@@ -6,10 +6,10 @@ int main(){
     cin >> input;
 
     for(int i=0; i<input.length(); i++){
-        if(input[i] == input[input.length()-(i+1)]){
-            cout << "1";
+        if(input[i] != input[input.length()-(i+1)]){
+            cout << "0";
             return 0;
         }
-        cout << "1";
     }
+    cout << "1";
 }
