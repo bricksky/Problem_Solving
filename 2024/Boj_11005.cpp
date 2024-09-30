56ef2947d49f260408ecb44ba365fb45fc62616b
bricksky <skydh1214@naver.com>
2024-09-30 23:06:38 +0900

    240930

---
+++ 2024/Boj_11005.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+  int n, b;
+  string result;
+
+  cin >> n >> b;
+
+  while(n){
+    if(n % b >= 0 && n % b <=9){
+        result += char(n % b + 48);
+    } else {
+        result += char((n % b + 'A')-10);
+    } n /= b;
+  }
+
+  for(int i = result.length()-1; i>=0; i--){
+    cout << result[i];
+  }
+
+  return 0;
+}
+
+
+
