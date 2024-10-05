c70145c025482422a0110b75541ded010b70eafd
bricksky <skydh1214@naver.com>
2024-10-05 20:57:46 +0900

    241005

---
+++ 2024/Boj_1978.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n=0;        // 입력받을 수의 개수
+    int number= 0;  // 입력받을 n개이 수
+    int count=0;    // 소수인 입력받은 수..?
+    int result = 0;    // 최종 소수의 수
+
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        cin >> number ;
+        for(int div=1; div<=number; div++){
+            if(number % div == 0){
+                count++;
+            }
+        }
+        if(count == 2){
+            result++;
+        }
+         count = 0;
+    }
+    cout << result << endl;
+}
