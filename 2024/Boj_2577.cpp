c25660986079383a317220f5414abaf554284742
bricksky <skydh1214@naver.com>
2024-10-09 20:44:57 +0900

    241009

---
+++ 2024/Boj_2577.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b, c;
+    int result;
+    int arr[10] = {0};
+
+    cin >> a >> b >> c;
+    result = a * b * c;
+
+    while(result>0){
+        arr[result % 10]++;
+        result = result / 10;
+    }
+
+    for(int i=0; i<10; i++){
+        cout << arr[i] << endl;
+    }
+
+    return 0;
+}
