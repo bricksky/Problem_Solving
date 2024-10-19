8235ff5b6d364ac8116d248593b1ac68e70d9da7
bricksky <skydh1214@naver.com>
2024-10-19 22:32:07 +0900

    241019

--- 2024/Boj_14125.cpp
+++ 2024/Boj_14125.cpp
@@ -16,7 +16,7 @@ int main(){
 
     // 삼각형 조건에 부합하지 않는 경우
     else if(arr[2] = arr[0] + arr[1]-1){
-        result = arr[0] + arr[1] + arr[2]
+        result = arr[0] + arr[1] + arr[2];
     }
 
     cout << result;
