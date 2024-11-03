02ce487213a6f00584883f4d4a25122d624c33e0
bricksky <skydh1214@naver.com>
2024-11-03 15:59:06 +0900

    241103

---
+++ 2024/Boj_2417.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    long long n;
+    long long q;
+
+    cin >> n;
+
+    q = sqrt(n);
+
+    if((q*q)<n){
+        ++q;
+    } 
+    cout << q << '\n';
+
+}
