3aa6e6ff662a61b8feee250431c0e8b2a3cc9e37
bricksky <skydh1214@naver.com>
2024-11-04 22:42:01 +0900

    241104

---
+++ 2024/Boj_3986.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int result = 0;
+    while(n--){
+        stack<char> stack;
+        string word;
+        cin >> word;
+
+        for(int i=0; i<word.length(); i++){
+            if(stack.empty()){
+                stack.push(word[i]);
+            } else {
+                if(stack.top() == word[i]){
+                    stack.pop();
+                } else {
+                    stack.push(word[i]);
+                }
+            }
+        }
+        if(stack.empty()){
+            result++;
+        }
+    }
+    cout << result;
+}
