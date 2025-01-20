6ee5fd56585f4562609c27455d34d602621fab80
bricksky <skydh1214@naver.com>
2025-01-20 20:37:01 +0900

    250120

---
+++ 2025/Boj_1251.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+
+    int n = s.size();
+    string result(n, 'z');
+
+    for(int i=1; i<n-1; i++){
+        for(int j=i+1; j<n; j++){
+            string a = s.substr(0, i);
+            string b = s.substr(i, j-i);
+            string c = s.substr(j, n-j);
+
+            reverse(a.begin(), a.end());
+            reverse(b.begin(), b.end());
+            reverse(c.begin(), c.end());
+
+            string combined = a + b + c;
+
+            if(combined < result){
+                result = combined;
+            }
+        }
+    }
+    cout << result << endl;
+    return 0;
+}
