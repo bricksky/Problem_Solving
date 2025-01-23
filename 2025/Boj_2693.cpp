4bd8b64f3f08b12aacc08a93b3234181667dd9d6
bricksky <skydh1214@naver.com>
2025-01-23 16:22:07 +0900

    250123

--- 2025/Boj_2693.cpp
+++ 2025/Boj_2693.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/* #include <iostream>
 #include <algorithm>
 
 using namespace std;
@@ -10,11 +10,15 @@ int main(){
     cin >> t;
 
     for(int i=0; i<t; i++){
+
+        // 각 테스트 케이스마다 10개의 숫자를 입력받음
         for(int j=0; j<10; j++){
             cin >> arr[j];
         }
+        
+        // 내림차순으로 정렬
         sort(arr, arr+10, greater<int>());
         cout << arr[2] << endl;
     }
     return 0;
-}
+} */
