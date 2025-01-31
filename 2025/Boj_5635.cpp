85548bf672f12e61aa720e02c2f38af79753a0d3
bricksky <skydh1214@naver.com>
2025-01-31 22:28:13 +0900

    250131

---
+++ 2025/Boj_5635.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    vector<pair<pair<int, int>, pair<int, string> > > v(n);
+                    // 연도, 월, 일, 이름
+    for(int i=0; i<n; i++){
+        cin >> v[i].second.second >> v[i].second.first >> v[i].first.second >> v[i].first.first; 
+    }
+
+    sort(v.begin(), v.end());
+
+    cout << v[0].second.second << '\n' << v[n-1].second.second << '\n';
+    return 0;
+}
