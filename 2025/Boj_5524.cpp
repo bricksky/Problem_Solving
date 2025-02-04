9b49796170b27f117fe676431a724de4ff3df050
bricksky <skydh1214@naver.com>
2025-02-04 17:21:53 +0900

    250204

---
+++ 2025/Boj_5524.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    string s;
+
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        cin >> s;
+        for(int i=0; i<s.length(); i++){
+            s[i] = tolower(s[i]);
+        }
+        cout << s << '\n';
+    }
+
+}
