3884a1dc0aefd50334bedf2a44f1e4b13e909a8d
bricksky <skydh1214@naver.com>
2025-03-28 22:53:21 +0900

    250328

--- 2025/Boj_2739.cpp
+++ 2025/Boj_2739.cpp
@@ -7,7 +7,7 @@ int main(){
     cin >> N;
 
     for(int i=1; i<=9; i++){
-        cout << N << " * " << i << " = " << N * i;
+        cout << N << " * " << i << " = " << N * i << endl;
     }
     return 0;
 }
