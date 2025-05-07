f9dce3da16d046e050fa5d524438bb624706202c
bricksky <skydh1214@naver.com>
2025-05-07 11:29:48 +0900

    settings

--- 2024/Boj_10989.cpp
+++ 2024/Boj_10989.cpp
@@ -1,27 +1,3 @@
-// #include <iostream>
-// #include <algorithm>
-// #include <vector>
-
-// using namespace std;
-
-// int main(){
-//     int n;
-//     cin >> n;
-
-//     vector<int> numbers(n);
-
-//     for(int i=0; i<n; i++){
-//         cin >> numbers[i];
-//     }
-
-//     sort(numbers.begin(), numbers.end());
-
-//     for(int i=0; i<n; i++){
-//         cout << numbers[i] << '\n';
-//     }
-//     return 0;
-// }
-
 #include <iostream>
 using namespace std;
 
