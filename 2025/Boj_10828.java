af6ecffcfd692ce734e715b1980a0e5ee033ff61
bricksky <skydh1214@naver.com>
2025-05-07 18:15:08 +0900

    250507

---
+++ 2025/Boj_10828.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Boj_10828 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Stack<Integer> stack = new Stack<>();
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            String input = scanner.next();
+
+            switch (input) {
+                case "push":
+                    int num = scanner.nextInt();
+                    stack.push(num);
+                    break;
+                case "pop":
+                    if (stack.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(stack.pop());
+                    }
+                    break;
+                case "size":
+                    System.out.println(stack.size());
+                    break;
+                case "empty":
+                    if (stack.isEmpty()) {
+                        System.out.println("1");
+                    } else {
+                        System.out.println("0");
+                    }
+                    break;
+                case "top":
+                    if (stack.isEmpty()) {
+                        System.out.println("-1");
+                    } else {
+                        System.out.println(stack.peek());
+                    }
+                    break;
+            }
+        }
+    }
+}
