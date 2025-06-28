2d527911409147dd978937e5d982a3a21924d918
bricksky <skydh1214@naver.com>
2025-06-28 17:44:30 +0900

    250628

---
+++ 2025/2025-java/src/Boj_1927.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Boj_1927 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        for(int i=0; i<n; i++){
+            int x = Integer.parseInt(br.readLine());
+
+            if(x==0){
+                if(queue.isEmpty())
+                    sb.append(0).append("\n");
+                else{
+                    sb.append(queue.poll()).append("\n");
+                }
+            } else {
+                queue.add(x);
+            }
+        }
+        System.out.println(sb);
+        br.close();
+    }
+}
+
