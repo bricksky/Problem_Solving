55a51d74f6d72a9f207857c3352075e69fa8e484
bricksky <skydh1214@naver.com>
2025-07-01 23:48:11 +0900

    250701

---
+++ 2025/2025-java/src/Boj_1715.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Boj_1715 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < n; i++) {
+            pq.add(Integer.parseInt(br.readLine()));
+        }
+
+        int total = 0;
+
+        while (pq.size() > 1) {
+            int a = pq.poll();
+            int b = pq.poll();
+
+            int sum = a + b;
+            total += sum;
+
+            pq.add(sum);
+        }
+        System.out.println(total);
+    }
+}
