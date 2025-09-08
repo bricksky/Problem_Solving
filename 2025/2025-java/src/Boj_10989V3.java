9c6a4a4e5bc1d8cbe20796032667249f253d33a5
bricksky <skydh1214@naver.com>
2025-09-08 10:47:57 +0900

    250908

---
+++ 2025/2025-java/src/Boj_10989V3.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Boj_10989V3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] count = new int[10001];
+
+        for (int i = 0; i < n; i++) {
+            count[Integer.parseInt(br.readLine())]++;
+        }
+
+        for (int i = 1; i <= 1000; i++) {
+            while (count[i]-- > 0) {
+                bw.write(i + "\n");
+            }
+        }
+        bw.flush();
+    }
+}
