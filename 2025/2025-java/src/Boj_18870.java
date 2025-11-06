af936ba13428798dce06ee4f7182d1c922a53046
bricksky <skydh1214@naver.com>
2025-11-06 11:25:29 +0900

    251106

--- 2025/2025-java/src/Boj_18870.java
+++ 2025/2025-java/src/Boj_18870.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Boj_18870 {
