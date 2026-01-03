ea14596c5013e12587e3e510fefd4b066648cf09
bricksky <skydh1214@naver.com>
2026-01-03 17:16:43 +0900

    260103

---
+++ 2026/Boj_15656.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Boj_15656 {
+
+    static int n, m;
+    static int[] numbers;
+    static boolean[] check;
+    static int[] output;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+        numbers = new int[n];
+        check = new boolean[n];
+        output = new int[m];
+
+        for (int i = 0; i < n; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        Arrays.sort(numbers);
+        perm(0);
+        System.out.println(sb);
+    }
+
+    public static void perm(int depth) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(output[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            output[depth] = numbers[i];
+            perm(depth + 1);
+        }
+    }
+}
