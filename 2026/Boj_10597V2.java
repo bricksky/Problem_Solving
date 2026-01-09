ea13f9fd73990a65a00b681662859530a4a4f02c
bricksky <skydh1214@naver.com>
2026-01-09 11:16:14 +0900

    260109

---
+++ 2026/Boj_10597V2.java
@@ -0,0 +1,58 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Boj_10597V2 {
+    static int[] check = new int[101];
+    static char[] input;
+    static int n = 0;
+    static List<Integer> ans = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        input = sc.next().toCharArray();
+        n = input.length > 9 ? 9 + (input.length - 9) / 2 : input.length;
+        solve(0);
+    }
+
+    static void solve(int index) {
+        if (index >= input.length) {
+            for (Integer num : ans) {
+                System.out.print(num + " ");
+            }
+            System.exit(0);
+            return;
+        }
+
+        int target1 = atoi(input, index, 1);
+        if (target1 <= n && check[target1] == 0) {
+            check[target1] = index + 1;
+            ans.add(target1);
+            solve(index + 1);
+            ans.remove(ans.size() - 1);
+            check[target1] = 0;
+        }
+
+        if (index + 1 >= input.length) {
+            return;
+        }
+        int target2 = atoi(input, index, 2);
+        if (target2 <= n && check[target2] == 0) {
+            check[target2] = index + 1;
+            ans.add(target2);
+            solve(index + 2);
+            ans.remove(ans.size() - 1);
+            check[target2] = 0;
+        }
+    }
+
+    static int atoi(char[] input, int start, int length) {
+        int result = 0;
+        for (int i = start; i < start + length; i++) {
+            result *= 10;
+            result += input[i] - '0';
+        }
+        return result;
+    }
+}
