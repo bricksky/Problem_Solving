264ade262d5d4732c8ce3e0c80b3d3600dffed5a
bricksky <skydh1214@naver.com>
2026-01-22 16:36:03 +0900

    260122

--- 2026/Boj_1947.java
+++ 2026/Boj_1947.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Boj_1941 {
+public class Boj_1947 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
