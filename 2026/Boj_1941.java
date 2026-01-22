3a0b057e95e6b2fec070bed919c7c3142fb43449
bricksky <skydh1214@naver.com>
2026-01-22 16:03:26 +0900

    260122

---
+++ 2026/Boj_1941.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj_1941 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        long[] input = new long[n + 1];
+
+        input[1] = 0;
+        if (n >= 2) {
+            input[2] = 1;
+
+            for (int i = 3; i <= n; i++) {
+                input[i] = (long) (i - 1) * (input[i - 1] + input[i - 2]) % 1000000000L;
+            }
+        }
+        System.out.println(input[n]);
+    }
+}
