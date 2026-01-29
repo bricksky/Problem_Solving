3b27be37142f85ec08c7a9fad73fd0bf04b80077
bricksky <skydh1214@naver.com>
2026-01-29 14:29:44 +0900

    260129

---
+++ 2026/Boj_24264.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj_24264 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Integer.parseInt(br.readLine());
+
+        System.out.println(n * n);
+        System.out.println(2);
+    }
+}
